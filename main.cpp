ae5969f5209589b1a63786d99642ba64ff6d928d
tomasbarak <barakcinthia@gmail.com>
2021-04-20 20:35:55 -0300

    First complete UI Design

--- main.cpp
+++ main.cpp
@@ -1,34 +1,94 @@
 #include <SFML/Graphics.hpp>
 int main()
 {
+    sf::Clock myClock; // starts the clock
+    bool showCursor = false;
     std::string user;
     sf::String playerInput;
     sf::Text playerText;
     sf::Font font;
 
-    sf::Text titleLabel;
+    sf::Text inputCursor;
 
     playerText.setFillColor(sf::Color::Black);
-    playerText.setOrigin(100, 25);
-    playerText.setPosition(475, 200);
 
-    titleLabel.setFillColor(sf::Color::Black);
-    titleLabel.setString("User:");
-    titleLabel.setOrigin(100, 25);
-    titleLabel.setPosition(350, 100);
+    inputCursor.setFillColor(sf::Color::Black);
+    inputCursor.setString("Nombre de usuario:   ");
+    inputCursor.setPosition(360.f, 201.f);
+
 
     font.loadFromFile("Montserrat-Regular.ttf");
 
     playerText.setFont(font);
-    titleLabel.setFont(font);
+    inputCursor.setFont(font);
 
     sf::RenderWindow window(sf::VideoMode(1000, 500), "Trivia game");
-    sf::CircleShape shape(100.f);
-    shape.setFillColor(sf::Color::Green);
     sf::RectangleShape rectangle(sf::Vector2f(1000.f, 500.f));
+
+    rectangle.setFillColor(sf::Color{ 255, 255, 255 });
+
+    sf::RectangleShape intputBackground(sf::Vector2f(1000.f, 500.f));
+
+
+    sf::FloatRect textRect = playerText.getLocalBounds();
+    playerText.setOrigin(textRect.left + textRect.width/2.0f,
+                         textRect.top  + textRect.height/2.0f);
+
+
+    sf::Image image;
+    image.loadFromFile("SoftVisionLogoNoBGSmoke.png");
+    sf::Texture texture;
+
+    texture.loadFromImage(image);  //Load Texture from image
+
+    sf::Sprite sprite;
+
+    sprite.setScale(0.125f, 0.125f);
+
+
+    sprite.setPosition(436, 0);
+
+    sprite.setTexture(texture);
+
+
+
     while (window.isOpen())
     {
+        playerText.setFillColor(sf::Color{ 150, 150, 150 });
+        if(myClock.getElapsedTime() >= sf::milliseconds(350))
+        {
+            myClock.restart();
+            showCursor = !showCursor;
+            if(showCursor)
+            {
+                if(user.size() < 1)
+                {
+                    playerText.setString(user+"|");
+                }
+                else
+                {
+                    playerText.setString(user+"_");
+                }
+            }
+            else
+            {
+                playerText.setString(user);
+            }
+        }
+        playerText.setPosition(sf::Vector2f(1000/2.0f,500/2.0f));
+        sf::FloatRect textRect2 = inputCursor.getLocalBounds();
+        inputCursor.setOrigin(textRect2.width,
+                              textRect2.top  + textRect2.height/2.0f);
+        inputCursor.setPosition(sf::Vector2f((1000/2.0f)+25,500/2.0f));
+
+        sf::FloatRect textRect = playerText.getLocalBounds();
+        playerText.setOrigin(textRect.left,
+                             textRect.top  + textRect.height/2.0f);
+        playerText.setPosition(sf::Vector2f(1000/2.0f,500/2.0f));
+
+        inputCursor.setPosition((1000/2.0), (500/2.0f));
         sf::Event event;
+
         while (window.pollEvent(event))
         {
             if (event.type == sf::Event::Closed)
@@ -68,12 +128,11 @@ int main()
             }
         }
 
-
-        shape.setPosition(400, 150);
         window.clear();
         window.draw(rectangle);
-        window.draw(titleLabel);
+        window.draw(sprite);
         window.draw(playerText);
+        window.draw(inputCursor);
         window.display();
     }
 
